e99f886e1a21cf2153a859d00cd965e059c519e0
tocque <tocque@sina.com>
2025-12-02 23:13:34 +0800

    add @ as path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import motaServerPlugin from './vite-plugin-mota-server'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
     }),
     motaServerPlugin(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   publicDir: 'public',
   server: {
     port: 3000,
